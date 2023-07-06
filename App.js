78300d13d3c25d4e40432b6981a7624741ec803e
zinyatou <127304542+zinyatou@users.noreply.github.com>
2023-07-06 19:52:28 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PlayersList from "./components/PlayersList";
+
+function App() {
+  return (
+    <div className="App"> 
+      <h1>Liste des joueurs FIFA</h1>
+      <PlayersList />
+    </div>
+  );
+}
+
+export default App;
